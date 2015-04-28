25a98cbfd71c5967def835576eac29d62ab7bcd9
Kristian Elset Boe <kristian.e.boe@gmail.com>
2015-04-28 15:46:42 +0200

    refined

--- Random/IsItSortable.py
+++ Random/IsItSortable.py
@@ -1,27 +1,26 @@
 def isItSortable(A):
     n = len(A)
     counter = 0
-    for i in range(n-1):
+    i = 0
+    while i < n - 1:
         print(i)
         if counter > 1:
             return False
-        if A[i] > A[i+1]:
+        if A[i] > A[i + 1]:
             counter += 1
-            for j in range(i,n-2):
+            for j in range(i, n - 2):
                 if A[j] < A[i]:
                     i = j
+                    i -= 1
                     break
+        i += 1
+    if counter > 1:
+        return False
     return True
 
 
-A = [1,2,3,4,5]
+A = [2, 1, 3, 4, 5]
 print(isItSortable(A))
 
-A = [1,3,2,4,5]
-print(isItSortable(A))
-
-A = [1,2,3,4,3,3,5]
-print(isItSortable(A))
-
-A = [5,4,3,2,1]
+A = [1, 1, 4, 3, 5, 9, 8]
 print(isItSortable(A))
